6872b0366287258326013145dee3a6d0139c6d2c
PageNakao <134197107+PageNakao@users.noreply.github.com>
2023-05-21 20:55:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 // スライドショーコンテナの要素を取得
 const slideshowElement = document.getElementById("slideshow");
 
-// 写真をランダムにシャッフルする関数
-function shufflePhotos() {
-  for (let i = photoUrls.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [photoUrls[i], photoUrls[j]] = [photoUrls[j], photoUrls[i]];
+// スライドショーの画像URLの配列
+const photoUrls = [];
+
+// 写真をアップロードする関数
+function uploadPhoto() {
+  const photoInput = document.getElementById("photoInput");
+  const files = photoInput.files;
+
+  if (files.length > 0) {
+    for (const file of files) {
+      const reader = new FileReader();
+      reader.onload = function(event) {
+        const photoDataUrl = event.target.result;
+        photoUrls.push(photoDataUrl);
+        showSlideshow();
+      };
+      reader.readAsDataURL(file);
+    }
   }
 }
 
 // スライドショーを表示する関数
 function showSlideshow() {
-  // 写真の順番をランダムにシャッフルする
-  shufflePhotos();
-
   // スライドショーのHTMLを生成
   const slideshowHtml = photoUrls.map(photoUrl => `<img src="${photoUrl}">`).join("");
   // スライドショーコンテナにHTMLを設定
   slideshowElement.innerHTML = slideshowHtml;
 }
-
-// 初回のスライドショー表示を実行
-showSlideshow();
