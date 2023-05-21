368691a6b73d152e26abd21085c22701d01fb892
PageNakao <134197107+PageNakao@users.noreply.github.com>
2023-05-21 20:10:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,57 @@ const CLIENT_ID = "666930798185-jvgutuj2ikd8t0dpkee0b3u2565acv8h.apps.googleuser
 const CLIENT_SECRET = "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";
 const REDIRECT_URI = "https://pagenakao.github.io/photoshowerpagenakao.github.io/";
 
+// 認証URLを生成する関数
+function generateAuthUrl() {
+  const scopes = ["https://www.googleapis.com/auth/photoslibrary"];
+  const params = {
+    client_id: CLIENT_ID,
+    redirect_uri: REDIRECT_URI,
+    response_type: "code",
+    scope: scopes.join(" "),
+  };
+  const authUrl = `https://accounts.google.com/o/oauth2/auth?${encodeQueryString(params)}`;
+  return authUrl;
+}
+
+// クエリ文字列をエンコードする関数
+function encodeQueryString(params) {
+  return Object.entries(params)
+    .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+    .join("&");
+}
+
+// 認証コードからアクセストークンを取得する関数
+function getAccessToken(code) {
+  const params = {
+    code: code,
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET,
+    redirect_uri: REDIRECT_URI,
+    grant_type: "authorization_code",
+  };
+
+  return fetch("https://oauth2.googleapis.com/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: encodeQueryString(params),
+  })
+    .then(response => response.json())
+    .then(data => data.access_token)
+    .catch(error => {
+      console.error("Error getting access token:", error);
+    });
+}
+
+// 認証コードの取得とアクセストークンの取得を行う関数
+function handleAuthCode(code) {
+  getAccessToken(code)
+    .then(accessToken => {
+      // アクセスト
+
+
 // スライドショーコンテナの要素を取得
 const slideshowElement = document.getElementById("slideshow");
 
